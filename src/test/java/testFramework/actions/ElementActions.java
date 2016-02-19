60574656b9a36aaa9944dbd728188266697563bc
Claudia Eisbrenner <claudia.eisbrenner@gmail.com>
2016-02-19 18:38:43 +0100

    Create ElementActions.java

---
+++ src/test/java/testFramework/actions/ElementActions.java
@@ -0,0 +1,17 @@
+package testFramework.actions;
+
+public enum ElementActions {
+        CLICK,
+        SET_TEXT,
+        HOVER_AND_CLICK,
+        ELEM_EXISTS_WHEN_HOVER,
+        ELEM_EXISTS,
+        ELEM_HAS_VALUE,
+        ELEM_HAS_TEXT,
+        ELEM_HAS_NOT_TEXT,
+        IS_AT,
+        SELECT,
+        TEXT_REMOVED,
+        SRC_CONTAINS_TEXT,
+        SRC_NOT_CONTAINS_TEXT
+}
